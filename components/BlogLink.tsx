b6e5a24810457ddfa194717959b249d4662c7813
jiko21 <daikikojima0531@icloud.com>
2020-05-24 18:19:34 +0900

    A: component for index page

---
+++ components/BlogLink.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import BlogDate from './Date';
+import { BlogInfo } from '~/lib/blog.ts';
+
+type Props = {
+  blogInfo: BlogInfo;
+};
+
+const BlogLink: React.FC<Props> = ({ blogInfo }) => {
+  return (
+    <>
+      <div className="blog-link-card">
+        <Link href="#">
+          <a>{blogInfo.title}</a>
+        </Link>
+        <br />
+        <BlogDate dateStr={blogInfo.date} />
+      </div>
+      <style jsx global>{`
+        .blog-link-card {
+          border-radius: 5px;
+          padding: 10px;
+          box-shadow: -2px -2px 5px #555, 3px -3px 5px rgba(255, 255, 255, 0.8);
+        }
+        a {
+          font-size: 1.5rem;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default BlogLink;
